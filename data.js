5ae71420f8ca0f1c210fcc50e593016699995e39
Missarik <103245968+Missarik@users.noreply.github.com>
2022-04-08 10:04:18 +0200

    Add files via upload

---
+++ data.js
@@ -0,0 +1,19 @@
+let data = [
+    {
+        "name": "Joe Mama",
+        "age": 60,
+        "username": "jb",
+        "password": "abc123",
+    },
+    {
+        "name": "Nephh",
+        "age": 69,
+        "username": "nonecknephh",
+        "password": "neck",
+    },{
+        "name": "Maxwel",
+        "age": 24,
+        "username": "mc20",
+        "password": "onlyscoresscreamers",
+    },
+]
